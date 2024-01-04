4ef49ea3642672dbff3afaed953203147177f25b
sravankumarmamidi <sravanmamidi1753@gmail.com>
2024-01-04 15:06:01 +0530

    third commit

---
+++ counter-project/src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react'
+
+import Clickevent from "./Clickevent"
+
+
+
+
+
+const App = () => {
+  return (
+    <div className= "">
+    <h1>Counter App is
+      <Clickevent />
+</h1>
+
+    </div>
+  );
+}
+
+export default App;
